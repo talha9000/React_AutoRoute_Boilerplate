89adf0b85ada1f6b58d40b4235c0dfe986ea73fd
muhammad.talha <muhammad.talha@odysseyanalytics.net>
2024-04-30 15:38:29 +0500

    some chanes   handle []

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
  import { Route,Routes } from "react-router-dom";
 import { lazy,Suspense } from "react";
+import Main from "./fixroute/Main";
  const PageLayout=lazy(()=>import('./container/PageLayout'))
 function App() {
   return (
   <>
   <Suspense fallback={<div className="text-center text-lg">Loading page....</div>}>
     <Routes>
+    <Route path="/" element={<Main/>}/>
     <Route path="/*" element={<PageLayout/>}/>
     </Routes>
   </Suspense>
