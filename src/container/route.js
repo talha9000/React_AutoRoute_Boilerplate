f6f7a9d58e55b57d0408b6dc351fa983fdbfe3b7
muhammad.talha <muhammad.talha@odysseyanalytics.net>
2024-04-30 11:42:33 +0500

    Boilerplate with subroutes define infolder

--- src/container/route.js
+++ src/container/route.js
@@ -1,22 +1,26 @@
 // route.js
 import { useEffect } from 'react';
 
-// Function to dynamically import files from the page folder
+// Function to recursively import files and folders from the page directory
 const importAll = (r) => {
-  return r.keys().map((key) => {
-    const component = r(key).default; // Assuming each file exports a default component
-    const path = key.substr(1).replace(/\/index\.js$/, '').replace(/\.js$/, ''); // Remove leading './', '/index.js', and '.js' from path
-    return { path, component };
-  });
+  const files = r.keys().map((key) => ({
+    path: key.substr(1).replace(/\/index\.js$/, '').replace(/\.js$/, '').toLowerCase(),
+    component: r(key).default
+  }));
+
+  const folders = r.keys().filter((key) => key.indexOf('/') !== -1 && key.endsWith('/')).map((folder) => {
+    const folderName = folder.substr(1).replace(/\/$/, '').toLowerCase();
+    const folderFiles = importAll(require.context(`../pages/${folderName}`, true, /\.js$/));
+    return folderFiles.map((file) => ({
+      path: `${folderName}/${file.path}`,
+      component: file.component
+    }));
+  }).flat();
+
+  return [...files, ...folders];
 };
 
-// Dynamically import all JavaScript files in the specified directory and its subdirectories
-const pageFiles = importAll(require.context('../pages', true, /\.js$/));
+// Dynamically import all JavaScript files and folders in the specified directory and its subdirectories
+const routes = importAll(require.context('../pages', true, /\.js$/));
 
-// Export the route array
-export const routes = pageFiles.map((file) => ({
-  path: file.path,
-  component: file.component,
-}));
-
- 
+export { routes };
